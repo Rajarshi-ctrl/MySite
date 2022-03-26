c4ccbd08ec8117d42db53e9e1b271a694d0e5d07
Rajarshi Dutta <rajarshidutta99@gmail.com>
2022-03-26 23:30:34 +0530

    procfile

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,9 @@ from me import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,5 +34,9 @@ urlpatterns = [
     path("project/", views.project, name="project"),
 
     path("show/", views.getContact),
+    
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
