6a9a766d9461b608d8a0270b894de6e072edd011
Rajarshi Dutta <rajarshidutta99@gmail.com>
2022-04-12 12:22:26 +0530

    add env

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,6 @@ from me import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
 
 
 urlpatterns = [
@@ -34,9 +32,5 @@ urlpatterns = [
     path("project/", views.project, name="project"),
 
     path("show/", views.getContact),
-    
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
